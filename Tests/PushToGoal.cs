9b7227e7cc4aa34dc109a7c919102dd9e27110ab
Abel0001 <abel.words1@gmail.com>
2023-12-22 20:08:52 +0100

    Asd

--- Tests/PushToGoal.cs
+++ Tests/PushToGoal.cs
@@ -1,21 +1,31 @@
+
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+
 public class PushToGoal
 {   
+
+
     static ObjectList objectList = new ObjectList(Program.screen);
-    static Map1 map1 = new Map1(Program.screen, objectList);
+    //static Map1 map1 = new Map1(Program.screen, objectList);
     static GameObject player = new GameObject("Player", Program.screen, 1, objectList, true, true,false,5,5);
-   // static GameObject box = new GameObject("Box", Program.screen, 1, objectList, true, false,true, 7,7);
-   // static GameObject pushBox = new GameObject("Push", Program.screen, 2, objectList, true, true, true,9,9);
+    static GameObject box = new GameObject("Box", Program.screen, 1, objectList, true, false,true, 7,7);
+    static GameObject pushBox = new GameObject("Push", Program.screen, 2, objectList, true, true, true,9,9);
 
 
     public static void StartGame(){
         player.Summon();
-        map1.LoadMap();
-      //  box.Summon();
-     //   pushBox.Summon();
+        
+       // map1.LoadMap();
+        
+        box.Summon();
+        pushBox.Summon();
         GameLoop();
     }
 
     public static void GameLoop(){
+        foreach(GameObject gameObject in objectList.GetGameObjects()) Console.Write(gameObject.NameId);
+
         while(true){
             MovePlayer();
         }
